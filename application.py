176320205c025baff59789d6a0309b1191751605
tskluzac <skluzacek@uchicago.edu>
2019-11-11 11:43:35 -0600

    debug in EB.

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
 
 from flask import Flask, request
 
-from status_checks import get_crawl_status# , get_extract_status
-from container_lib.xtract_matio import MatioExtractor
+from status_checks import get_crawl_status # , get_extract_status
+# from container_lib.xtract_matio import MatioExtractor
 from crawlers.globus_base import GlobusCrawler
 from uuid import uuid4
 import json
@@ -18,12 +18,12 @@ def crawl_launch(crawler, tc):
     return "done"
 
 
-def extract_launch(mex):
-    mex.send_files()
-
-
-def results_poller_launch():
-    print("HI. ")
+# def extract_launch(mex):
+#     mex.send_files()
+#
+#
+# def results_poller_launch():
+#     print("HI. ")
 
 
 @application.route('/')
@@ -65,27 +65,27 @@ def get_cr_status():
     return resp
 
 
-@application.route('/extract', methods=['POST'])
-def extract_mdata():
-
-    r = request.json
-    crawl_id = r["crawl_id"]
-    headers = json.loads(r["headers"])
-    print(headers)
-
-    mex = MatioExtractor(eid='e3a377f9-d046-41af-956d-141121ccf712', crawl_id=crawl_id, headers=headers)
-
-    print("SENDING FILES...")
-    # threading.Thread(target=extract_launch, args=([1,2,3]))
-    mex.send_files()
-
-    print("POLLING RESPONSES...")
-    # TODO: This needs to happen in its own thread.
-    mex.poll_responses()
-
-    extract_id = str(uuid4())
-
-    return extract_id
+# @application.route('/extract', methods=['POST'])
+# def extract_mdata():
+#
+#     r = request.json
+#     crawl_id = r["crawl_id"]
+#     headers = json.loads(r["headers"])
+#     print(headers)
+#
+#     mex = MatioExtractor(eid='e3a377f9-d046-41af-956d-141121ccf712', crawl_id=crawl_id, headers=headers)
+#
+#     print("SENDING FILES...")
+#     # threading.Thread(target=extract_launch, args=([1,2,3]))
+#     mex.send_files()
+#
+#     print("POLLING RESPONSES...")
+#     # TODO: This needs to happen in its own thread.
+#     mex.poll_responses()
+#
+#     extract_id = str(uuid4())
+# 
+#     return extract_id
 
 
 # @application.route('/get_extract_status', methods=['GET'])
