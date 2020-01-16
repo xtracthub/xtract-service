4e64f93651895b33d9c4a3fa0219a2a370328c87
tskluzac <skluzacek@uchicago.edu>
2020-01-15 18:29:32 -0600

    Trying to get rid of some log entries for now.

--- crawlers/groupers/matio_grouper.py
+++ crawlers/groupers/matio_grouper.py
@@ -12,21 +12,23 @@ class MatIOGrouper:
         # https://github.com/materials-data-facility/MaterialsIO/blob/master/materials_io/utils/interface.py
 
         # TODO [Enhancement]: Get these automatically using Stevedore (or get_all_parsers func?)
-        parser_desc = get_available_parsers()
-        parsers = [*parser_desc]  # This is a lis of all applicable parsers.
+        # parser_desc = get_available_parsers()
+        # parsers = [*parser_desc]  # This is a lis of all applicable parsers.
 
         group_coll = {}
 
-        for parser in parsers:
+        # for parser in parsers:
 
-            # TODO: Uncomment this to add back the other groups.
-            if parser not in ['dft']:
-                # if parser in ['noop', 'generic']:
-                continue
+        # TODO: Uncomment this to add back the other groups.
+        # if parser not in ['dft']:
+        #     # if parser in ['noop', 'generic']:
+        #     continue
 
-            p = get_parser(parser)
-            group = p.group(file_ls)
+        # p = get_parser(parser)
+        p = get_parser('dft')
+        group = p.group(file_ls)
 
-            group_coll[parser] = group
+        # group_coll[parser] = group
+        group_coll['dft'] = group
 
         return group_coll
