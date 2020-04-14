e29f822e620015fcfe5ec006f1af53877be79ad5
tskluzac <skluzacek@uchicago.edu>
2020-04-14 13:57:21 -0500

    Fixed. Can close issue.

--- status_checks.py
+++ status_checks.py
@@ -1,6 +1,7 @@
 
 import time
 from utils.pg_utils import pg_conn
+from datetime import datetime
 
 
 def get_crawl_status(crawl_id):
@@ -8,16 +9,32 @@ def get_crawl_status(crawl_id):
     conn = pg_conn()
     cur1 = conn.cursor()
     cur2 = conn.cursor()
+    cur3 = conn.cursor()
     count_query = f"SELECT COUNT(*) FROM groups WHERE crawl_id='{crawl_id}';"
-    status_query = f"SELECT status FROM crawls WHERE crawl_id='{crawl_id}';"
+    status_query = f"SELECT status, started_on, ended_on FROM crawls WHERE crawl_id='{crawl_id}';"
+    bytes_files_count_query = f"SELECT SUM(total_files), SUM(total_size) from families WHERE crawl_id='{crawl_id}';"
 
     cur1.execute(count_query)
     cur2.execute(status_query)
+    cur3.execute(bytes_files_count_query)
 
     count_val = cur1.fetchall()[0][0]
-    status_val = cur2.fetchall()[0][0]
+    status, started_at, ended_at = cur2.fetchall()[0]
+    files_processed, bytes_processed = cur3.fetchall()[0]
 
-    return {"groups_crawled": count_val, "crawl_status": status_val}
+    if status == "complete":
+        elapsed_time = ended_at - started_at
+    else:
+        elapsed_time = datetime.now() - started_at
+
+    total_seconds = elapsed_time.total_seconds()
+
+    return {"groups_crawled": count_val,
+            "crawl_status": status,
+            "elapsed_time": total_seconds,
+            "files_processed": files_processed,
+            "bytes_processed": int(bytes_processed)
+            }
 
 
 def get_extract_status(crawl_id):
