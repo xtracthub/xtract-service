a2a12845b194b21c352b1d81cbfb023e671b3063
tskluzac <skluzacek@uchicago.edu>
2020-06-13 12:49:03 -0500

    Keywords extracting in new extractor format.

--- keyword_test.py
+++ keyword_test.py
@@ -1,19 +1,12 @@
 
 import funcx
 import time
-import json
-import os
-import requests
-from container_lib.xtract_matio import serialize_fx_inputs
-from container_lib.xtract_keyword import keyword_extract
+from extractors.xtract_tabular import tabular_extract
 from fair_research_login import NativeClient
 from funcx.serialize import FuncXSerializer
 from queue import Queue
 from mdf_matio.validator import MDFValidator
-from materials_io.utils.interface import ParseResult
-from typing import Iterable, Set, List
 from functools import reduce, partial
-from mdf_matio.grouping import groupby_file, groupby_directory
 
 
 from mdf_toolbox import dict_merge
@@ -50,16 +43,6 @@ burst_size = 5
 
 batch_size = 5
 
-container_id = fxc.register_container(location='039706667969.dkr.ecr.us-east-1.amazonaws.com/xtract-keyword:latest',
-                                      container_type='docker',
-                                      name='kube-keyword',
-                                      description='I don\'t think so!')
-fn_id = fxc.register_function(keyword_extract,
-                              container_uuid=container_id,
-                              description="A sum function")
-
-
-print(f"Function UUID: {fn_id}")
 
 # Get the Headers....
 client = NativeClient(client_id='7414f0b4-7d05-4bb6-bb00-076fa3f17cf5')
@@ -72,8 +55,6 @@ tokens = client.login(
     no_local_server=True,
     no_browser=True)
 
-# print(tokens)
-# exit()
 
 auth_token = tokens["petrel_https_server"]['access_token']
 transfer_token = tokens['transfer.api.globus.org']['access_token']
@@ -83,13 +64,9 @@ funcx_token = tokens['funcx_service']['access_token']
 headers = {'Authorization': f"Bearer {funcx_token}", 'Transfer': transfer_token, 'FuncX': funcx_token, 'Petrel': mdf_token}
 print(f"Headers: {headers}")
 
-# NCSA file
-old_mdata = {"files": ["/mdf_open/kearns_biofilm_rupture_location_v1.1/Biofilm Images/Paper Images/Isofluence Images (79.4)/THY+75mM AA/1.jpg"]}
 
-# TODO: bring back
-# data = {"inputs": [], "transfer_token": transfer_token, "source_endpoint": 'e38ee745-6d04-11e5-ba46-22000b92c6ec',
-#         "dest_endpoint": globus_ep}
-data = {}
+data = {"inputs": [], "transfer_token": transfer_token, "source_endpoint": 'e38ee745-6d04-11e5-ba46-22000b92c6ec',
+        "dest_endpoint": globus_ep}
 
 
 SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly', 'https://www.googleapis.com/auth/drive']
@@ -117,16 +94,14 @@ def do_login_flow():
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
-    return creds, None  # Returning None because Tyler can't figure out how he wants to structure this yet.
+    return creds
 
 # THIS should force-open a Google Auth window in your local browser. If not, you can manually copy-paste it.
 auth_creds = do_login_flow()
-
-data["gdrive"] = auth_creds[0]
-data["file_id"] = "1u1bQtdFNmSk8UJ-O4VJSEbUX25aK6LkwNFVcwhwv4WE"
-data["is_gdoc"] = False
-data["extension"] = "pdf"
-# data = {'gdrive_pkl': b'\x80\x03cgoogle.oauth2.credentials\nCredentials\nq\x00)\x81q\x01}q\x02(X\x05\x00\x00\x00tokenq\x03X\xab\x00\x00\x00ya29.a0AfH6SMDkmvH9GI9EL6eKYaQo5OUF4c9Um3oIqgel5IN-Fc9nJqIbke9PTGPc3BuJvWhSuKuWT9NnXZmy9PIN1xMi7rkhCH_gY8bQttlPUEkBJ7HoKi_xPj1nU_pd3qQMnQvyV7oi_CeAaf_LTBrsK22x--WK7wA5cbjFq\x04X\x06\x00\x00\x00expiryq\x05cdatetime\ndatetime\nq\x06C\n\x07\xe4\x06\x08\x152\x1f\x08\x89\x05q\x07\x85q\x08Rq\tX\x0e\x00\x00\x00_refresh_tokenq\nXg\x00\x00\x001//0fYtW4N1qkLF2CgYIARAAGA8SNwF-L9Ir8QaekPkzMwhqN8orPQxeV_ayzLVwFmIHDsW4fmVRU_pj2zJZew1j3dqSY_XywTP_S3gq\x0bX\t\x00\x00\x00_id_tokenq\x0cNX\x07\x00\x00\x00_scopesq\r]q\x0e(X7\x00\x00\x00https://www.googleapis.com/auth/drive.metadata.readonlyq\x0fX%\x00\x00\x00https://www.googleapis.com/auth/driveq\x10eX\n\x00\x00\x00_token_uriq\x11X#\x00\x00\x00https://oauth2.googleapis.com/tokenq\x12X\n\x00\x00\x00_client_idq\x13XH\x00\x00\x00364500245041-r1eebsermd1qp1qo68a3qp09hhpc5dfi.apps.googleusercontent.comq\x14X\x0e\x00\x00\x00_client_secretq\x15X\x18\x00\x00\x00XMGaOYAeBxLz9ZWvsVQCvWtkq\x16X\x11\x00\x00\x00_quota_project_idq\x17NubN\x86q\x18.', 'file_id': '1zAJJy4bFQ2ZANV7W3iv7WdpZWRBp8iEN'}
+data["gdrive"] = auth_creds
+data["file_id"] = "1nuPQE15_5inIe6exuqaZ_Qd7DjBEJRBsIXOYKjURSRY"
+data["is_gdoc"] = True
+data["extension"] = "alksdjf"
 
 
 id_count = 0
@@ -134,76 +109,30 @@ group_count = 0
 groups_in_family = 1
 family = {"family_id": "test-family", "files": {}, "groups": {}}
 
-for i in range(groups_in_family):
-    group = {'group_id': group_count, 'files': [], 'parser': 'image'}
-    group_count += 1
-
-    # Here we populate the groups.
-    for f_obj in old_mdata["files"]:
-        # file_url = f'https://e38ee745-6d04-11e5-ba46-22000b92c6ec.e.globus.org{f_obj}'
-        file_url = f'https://data.materialsdatafacility.org{f_obj}'
-        print(file_url)
-        payload = {
-            'url': file_url,
-            'headers': headers, 'file_id': id_count}
-        id_count += 1
-        # Here we add file to family list if it doesn't already exist there.
-        if file_url not in family['files']:
-            family['files'][file_url] = payload
-        group['files'].append(file_url)
-    family["groups"][group_count-1] = group
-
-# data["inputs"].append(family)
+data["inputs"].append(family)
 
 task_dict = {"active": Queue(), "pending": Queue(), "results": [], "failed": Queue()}
 t_launch_times = {}
 
-assert(n_tasks % burst_size == 0)
+### TODO: New stuff.
+from extractors.xtract_keyword import KeywordExtractor
+keyx = KeywordExtractor()
+uuid = keyx.register_function()
 
-for n in range(int(n_tasks/burst_size)):
-    print(f"Processing burst: {n}")
+task_id = keyx.remote_extract_solo(event=data, fx_eid=fx_ep, headers=headers)
+task_dict["active"].put(task_id)
 
-    for i in range(burst_size):
-        print(headers)
-        print(data)
-        # exit()
-        res = requests.post(url=post_url,
-                            headers=headers,
-                            json={'endpoint': fx_ep,
-                                  'func': fn_id,
-                                  'payload': serialize_fx_inputs(
-                                      event=data)
-                                  }
-                            )
 
-        print(res.content)
-        if res.status_code == 200:
-            task_uuid = json.loads(res.content)['task_uuid']
-            task_dict["active"].put(task_uuid)
-            t_launch_times[task_uuid] = time.time()
-        print(i)
+print(f"TASK ID!: {task_id}")
 
-    # TODO: Add ghetto-retry for pending tasks to catch lost ones.
-    # TODO x10000: move this logic
-    timeout = 120
-    failed_counter = 0
-    while True:
-
-        if task_dict["active"].empty():
-            print("Active task queue empty... sleeping... ")
-            time.sleep(0.5)
-            break  # This should jump out to main_loop
-
-        cur_tid = task_dict["active"].get()
-        print(cur_tid)
-        status_thing = requests.get(get_url.format(cur_tid), headers=headers).json()
-
-        if 'result' in status_thing:
-            result = fx_ser.deserialize(status_thing['result'])
-            print(f"Result: {result}")
-
-        elif 'exception' in status_thing:
-            print(f"Exception: {fx_ser.deserialize(status_thing['exception'])}")
-            # break
-        else:
-            task_dict["active"].put(cur_tid)
+while True:
+    status_thing = requests.get(get_url.format(task_id), headers=headers).json()
+    print(status_thing)
+    if 'result' in status_thing:
+        result = fx_ser.deserialize(status_thing['result'])
+        print(f"Result: {result}")
+        break
+    elif 'exception' in status_thing:
+        print(f"Exception: {fx_ser.deserialize(status_thing['exception'])}")
+        break
+    time.sleep(1)
