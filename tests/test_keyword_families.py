e56a4f9e12e6c8584760e8ef1b090c8de1cdbdde
tskluzac <skluzacek@uchicago.edu>
2020-09-03 10:59:45 -0500

    [Working] committing so I can merge/test Will's extractor edits.

--- tests/test_keyword_families.py
+++ tests/test_keyword_families.py
@@ -29,7 +29,6 @@ fam_2 = Family()
 fam_1.add_group(files=[{"path": tab_1, "is_gdoc": True, "mimeType": "text/plain", "metadata": {}}], parser='text')
 fam_2.add_group(files=[{"path": tab_2, "is_gdoc": False, "mimeType": "application/pdf", "metadata": {}}], parser='text')
 
-
 # Full version of file throwing error
 # fam_2.add_group = {'files': [{'path': '1eTSkC46GUN4z-6mhFTJHsE9Z9EpmllMpCzdm-LLpuCQ', 'metadata': {'id': '1eTSkC46GUN4z-6mhFTJHsE9Z9EpmllMpCzdm-LLpuCQ', 'name': 'DCSL Info Session 1/24', 'mimeType': 'application/vnd.google-apps.presentation', 'webViewLink': 'https://docs.google.com/presentation/d/1eTSkC46GUN4z-6mhFTJHsE9Z9EpmllMpCzdm-LLpuCQ/edit?usp=drivesdk', 'shared': True, 'size': 0, 'is_gdoc': True, 'user_is_owner': False, 'shared_user_ids': ['03530326841615676301', '03711339970616793712', '09490290160418701680', '14453663582181058099', '09932392942899452680', '02136978862942460576', '12120473023176343867', '08122454148025534369'], 'parent': '1Qx9X7LzkMLdrEZNKZDTgQvjaZgxdqQBx', 'last_modified': 1591778133.558, 'extension': '', 'extractor': 'text'}, 'is_gdoc': True, 'mimeType': None, 'file_id': '2ed94934-2e41-49eb-a33f-ebe6698ee5ca'}, {'path': '1MEgpv9BO3yPbX0MPFy617OxM_SeIcIHp03DL3suQ7ZM', 'metadata': {'id': '1MEgpv9BO3yPbX0MPFy617OxM_SeIcIHp03DL3suQ7ZM', 'name': 'Learning Agreement | CDAC Summer Lab', 'mimeType': 'application/vnd.google-apps.document', 'webViewLink': 'https://docs.google.com/document/d/1MEgpv9BO3yPbX0MPFy617OxM_SeIcIHp03DL3suQ7ZM/edit?usp=drivesdk', 'shared': True, 'size': 0, 'is_gdoc': True, 'user_is_owner': False, 'shared_user_ids': ['03530326841615676301', '14453663582181058099', '09932392942899452680', '02136978862942460576', '12120473023176343867', '08122454148025534369'], 'parent': '1hVe6F5v_YduMMycSYKAR2BD7aEVDek0P', 'last_modified': 1591766119.419, 'extension': '', 'extractor': 'text'}, 'is_gdoc': True, 'mimeType': 'text/plain', 'file_id': '5d04e8a3-22eb-4d81-a4d5-60b84c327155'}]}
 fam_2.add_group = {'files': [{'path': '1eTSkC46GUN4z-6mhFTJHsE9Z9EpmllMpCzdm-LLpuCQ',
@@ -65,6 +64,7 @@ import time
 import requests
 from funcx.serialize import FuncXSerializer
 fx_ser = FuncXSerializer()
+exception_count = 0
 while True:
 
     get_url = 'https://api.funcx.org/v1/{}/status'
@@ -89,7 +89,13 @@ while True:
     elif 'exception' in status_thing:
         exception = fx_ser.deserialize(status_thing['exception'])
         print(f"Exception: {exception}")
-        exception.reraise()
+
+        try:
+            exception.reraise()
+        except Exception as e:
+            print(e)
+            exception_count += 1
+            print(f"Exception count: {exception_count}")
 
     else:
         task_dict["active"].put(cur_tid)
