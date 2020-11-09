03d37f7cd47153932ade6694b091aeea103983aa
tskluzac <skluzacek@uchicago.edu>
2020-11-09 16:58:01 -0600

    Updated. Pushing to cloud to get numbers for Zoa.

--- experiments/generate_crawl_csv.py
+++ experiments/generate_crawl_csv.py
@@ -3,6 +3,7 @@ import os
 import math
 import boto3
 import json
+import csv
 
 crawl_id = "7aff79a5-3536-4cc8-9092-37769056141c"
 
@@ -26,69 +27,75 @@ response = client.get_queue_url(
         )
 val_queue_url = response["QueueUrl"]
 
+with open("crawl_data.csv", "w") as f:
+    writer = csv.writer(f)
 
-for i in range(math.ceil(total_messages/10)):
+    for i in range(math.ceil(total_messages/10)):
 
-    # print(i)
-    # TODO:  Pull from the crawl_id queue.
-    sqs_response = client.receive_message(
-        QueueUrl=val_queue_url,
-        MaxNumberOfMessages=10,
-        WaitTimeSeconds=5)
-
-    delete_batch = []
-    message_batch = []
-    if len(sqs_response["Messages"]) > 0:
-
-        for i in range(len(sqs_response["Messages"])): 
-
-            # print(i)
-            message = sqs_response["Messages"][i]
-
-            id = message['MessageId']
-            body = message['Body']
-
-            # print(body)
-
-            # print(type(body))
-            # exit()
-
-            new_msg = {"Id": id, "MessageBody": body}
-            message_batch.append(new_msg)
-            family = json.loads(body)
-            print(family)
-
-            crawl_timestamp = family['metadata']['crawl_timestamp']
-            
-            # total_files = 0
-            total_file_size = 0
-            
-            
-            all_files = family['files']
-            total_files = len(all_files)
-            for file_obj in all_files:
-                total_file_size += file_obj['metadata']['physical']['size']
-                # print(file_size)
-
-
-            print(total_file_size)
-            print(total_files)
-            exit()
-
-        del_info = {'ReceiptHandle': message["ReceiptHandle"],
-                    'Id': message["MessageId"]}
-        delete_batch.append(del_info) 
-
-        
-
-        #response1 = client.send_message_batch(QueueUrl=test_queue_url,
-        #                                      Entries=message_batch)
-
-        #response2 = client.send_message_batch(QueueUrl=val_queue_url,
-        #                                      Entries=message_batch)
-
-        # We need to delete from the real queue because otherwise we'll continuously select same file
-        response = client.delete_message_batch(
+        # print(i)
+        # TODO:  Pull from the crawl_id queue.
+        sqs_response = client.receive_message(
             QueueUrl=val_queue_url,
-            Entries=delete_batch)
+            MaxNumberOfMessages=10,
+            WaitTimeSeconds=5)
+
+        delete_batch = []
+        message_batch = []
+        if len(sqs_response["Messages"]) > 0:
+
+            for i in range(len(sqs_response["Messages"])):
+
+                # print(i)
+                message = sqs_response["Messages"][i]
+
+                id = message['MessageId']
+                body = message['Body']
+
+                # print(body)
+
+                # print(type(body))
+                # exit()
+
+                new_msg = {"Id": id, "MessageBody": body}
+                message_batch.append(new_msg)
+                family = json.loads(body)
+                print(family)
+
+                crawl_timestamp = family['metadata']['crawl_timestamp']
+                family_id = family['family_id']
+
+                # total_files = 0
+                total_file_size = 0
+
+
+                all_files = family['files']
+                total_files = len(all_files)
+                for file_obj in all_files:
+                    total_file_size += file_obj['metadata']['physical']['size']
+                    # print(file_size)
+
+
+                print(total_file_size)
+                print(total_files)
+
+                writer.writerow([family_id, crawl_timestamp, total_files, total_file_size])
+
+                # exit()
+
+            del_info = {'ReceiptHandle': message["ReceiptHandle"],
+                        'Id': message["MessageId"]}
+            delete_batch.append(del_info)
+
+
+
+            #response1 = client.send_message_batch(QueueUrl=test_queue_url,
+            #                                      Entries=message_batch)
+
+            #response2 = client.send_message_batch(QueueUrl=val_queue_url,
+            #                                      Entries=message_batch)
+
+            # We need to delete from the real queue because otherwise we'll continuously select same file
+            response = client.delete_message_batch(
+                QueueUrl=val_queue_url,
+                Entries=delete_batch)
 
