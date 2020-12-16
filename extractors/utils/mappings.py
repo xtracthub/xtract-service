b2f247ed6d89e1a98c22c4ba9e1aa264b057638f
tskluzac <skluzacek@uchicago.edu>
2020-12-16 17:27:22 -0600

    Adding the new utils/batch mappings. Updating funcX function.

--- extractors/utils/mappings.py
+++ extractors/utils/mappings.py
@@ -5,10 +5,7 @@ mapping = {
         {'func_uuid': 'a30f90fd-15ad-47c6-bf93-b4f4f753d3a8',
          'location': 'xtract-matio.img',
          'container_type': 'singularity',
-         'ep_id': '71509922-996f-4559-b488-4588f06f0925',
+         'ep_id': '7faf21be-4667-447c-a96d-4dbe14875cf1',
          'data_path': '/project2/chard/skluzacek/inposoutcars/'},
     'xtract-matio::riverk8s': "TODO",
-    'prefetcher::midway2':
-    {
-        'func_uuid': '17214422-4570-4891-9831-2212614d04fa'}
 }
