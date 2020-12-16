8bac4c439e0be2cbbe4655570666f060ba116661
tskluzac <skluzacek@uchicago.edu>
2020-12-16 15:35:10 -0600

    Adding high-throughput tests.

--- tests/test_matio_local.py
+++ tests/test_matio_local.py
@@ -6,7 +6,12 @@ from extractors.xtract_matio import MatioExtractor
 
 from extractors.utils.mappings import mapping
 
+from queue import Queue
+
 import time
+import json
+
+import threading
 
 
 from xtract_sdk.packagers.family import Family
@@ -21,52 +26,182 @@ container_type = map['container_type']
 location = map['location']
 ep_id = map['ep_id']
 
-fam_1 = Family()
-fam_1.add_group(files=[{"path": f"{base_path}INCAR", "metadata": {}, "base_url": base_url}, {"path": f"{base_path}OUTCAR", "metadata": {}, "base_url": base_url}, {"path": f"{base_path}POSCAR", "metadata": {}, "base_url": base_url}], parser='dft')
-fam_1.family_id = "data_toprocess"
-# fam_1.headers = file_headers
-
-fam_batch = FamilyBatch()
-fam_batch.add_family(fam_1)
+# TODO: make sure this is proper size.
+map_size = 75
+batch_size = 50
 
 
-img_extractor = MatioExtractor()
+class test_orch():
+    def __init__(self):
+        self.current_tasks_on_ep = 0
+        self.max_tasks_on_ep = 80000
+        self.fxc = FuncXClient()
 
-fn_uuid = img_extractor.register_function(container_type=container_type, location=location,
-                                          ep_id=ep_id, group="a31d8dce-5d0a-11ea-afea-0a53601d30b5")
+        self.funcx_batches = Queue()
+        self.polling_queue = Queue()
 
+        self.num_poll_reqs = 0
+        self.num_send_reqs = 0
 
-def test(event):
-    return sum(event)
+        self.total_families_sent = 0
 
+        self.successes = 0
+        self.failures = 0
 
-def main(fxc, ep_id):
+        self.fam_batches = []
 
-    # while True:
-    # container1_uuid = fxc.register_container('xtract-matio.img', 'singularity')
-    # container2_uuid = fxc.register_container('/projects/CSC249ADCD01/skluzacek/xtract-images.img', 'singularity')
-    # print("Container UUID: {}".format(container1_uuid))
-    # fn_uuid = fxc.register_function(matio_extract,
-    #                                 ep_id, # TODO: We do not need ep id here
-    #                                 container_uuid=container1_uuid,
-    #                                 description="New sum function defined without string spec")
-    # fn_uuid = fxc.register_function(test, ep_id, description = "HA")
-    # print("FN_UUID : ", fn_uuid)
-    while True:
-        # res = fxc.run([1,2,3,99], endpoint_id=ep_id, function_id=fn_uuid)
-        res = fxc.run({'family_batch': fam_batch}, endpoint_id=ep_id, function_id=fn_uuid)
-        print(res)
-        for i in range(100):
+        big_json = "/Users/tylerskluzacek/PyCharmProjects/xtracthub-service/experiments/tyler_200k.json"
+
+        with open(big_json, 'r') as f:
+            self.fam_list = json.load(f)
+
+        self.start_time = time.time()
+
+        self.preproc_fam_batches()
+
+    def path_converter(self, family_id, old_path):
+        path_ls = old_path.split('/')
+        file_name = path_ls[-1]
+        new_path = f"/project2/chard/skluzacek/{family_id}/{file_name}"
+        return new_path
+
+    def preproc_fam_batches(self):
+        for fam in self.fam_list:
+
+            fam_batch = FamilyBatch()
+
+            while len(fam_batch.families) < batch_size:
+
+                # First convert to the correct paths
+                for file_obj in fam['files']:
+                    old_path = file_obj['path']
+                    new_path = self.path_converter(fam['family_id'], old_path)
+                    file_obj['path'] = new_path
+
+                for group in fam['groups']:
+                    for file_obj in group['files']:
+                        old_path = file_obj['path']
+                        new_path = self.path_converter(fam['family_id'], old_path)
+                        file_obj['path'] = new_path
+
+                empty_fam = Family()
+                empty_fam.from_dict(fam)
+                fam_batch.add_family(empty_fam)
+
+            self.fam_batches.append(fam_batch)
+
+        img_extractor = MatioExtractor()
+
+        self.fn_uuid = img_extractor.register_function(container_type=container_type, location=location,
+                                                  ep_id=ep_id, group="a31d8dce-5d0a-11ea-afea-0a53601d30b5")
+
+        current_batch = []
+        for fam_batch in self.fam_batches:
+            if len(current_batch) < batch_size:
+                current_batch.append(fam_batch)
+            else:
+                self.funcx_batches.put(current_batch)
+                current_batch = [fam_batch]
+
+        # Grab the stragglers.
+        if len(current_batch) > 0:
+            self.funcx_batches.put(current_batch)
+
+    # TODO: let the failures fail.
+    def send_batches_thr_loop(self):
+
+        while not self.funcx_batches.empty():
+
+            if self.current_tasks_on_ep > self.max_tasks_on_ep:
+                print(f"There are {self.current_tasks_on_ep}. Sleeping...")
+                time.sleep(5)
+                continue
+
+            batch = self.funcx_batches.get()
+            fx_batch = self.fxc.create_batch()
+
+            for item in batch:
+
+                fam_batch_size = len(item.families)
+
+                fx_batch.add({'family_batch': item}, endpoint_id=ep_id, function_id=self.fn_uuid)
+                self.current_tasks_on_ep += fam_batch_size
+
             try:
-                x = fxc.get_result(res)
-                print(x)
-                time.sleep(3)
-                break
-            except Exception as e:
-                print("Exception: {}".format(e))
-                time.sleep(2)
+                res = self.fxc.batch_run(fx_batch)
+                self.num_send_reqs += 1
+            except:
+                time.sleep(0.5)
+                continue
+
+            for tid in res:
+                self.polling_queue.put(tid)
+
+            time.sleep(1)
+
+    def polling_loop(self):
+        while True:
+
+            current_tid_batch = []
+            for i in range(500):  # TODO: 1000 might be too big?
+                if self.polling_queue.empty():
+                    print("Polling queue empty. Creating batch!")
+                    break
+                else:
+                    tid = self.polling_queue.get()
+                    current_tid_batch.append(tid)
+
+            if len(current_tid_batch) == 0:
+                print("Batch is empty. Sleeping... ")
+                time.sleep(5)
+            res = self.fxc.get_batch_status(current_tid_batch)
+            self.num_poll_reqs += 1
+
+            for item in res:
+                if 'result' in res[item]:
+                    # self.successes += 1
+                    print(res[item]['result'])
+                    ret_fam_batch = res[item]['result']['family_batch']
+
+                    fam_len = len(ret_fam_batch.families)
+                    self.successes += fam_len
+
+                    self.current_tasks_on_ep -= fam_len
+
+                    # NOTE -- we're doing nothing with the returned metadata here.
+
+                elif 'status' in res[item]:
+                    self.polling_queue.put(item)
+                else:
+                    print("*********ERROR *************")
+                    self.failures += 1
+                    print(res)
+
+    def stats_loop(self):
+        while True:
+            print("*********************************")
+            print(f"Num successes: {self.successes}")
+            print(f"Num failures: {self.failures}")
+            print(f"Only {self.current_tasks_on_ep} tasks at endpoint. ")
+
+            print(f"Number of send requests: {self.num_send_reqs}")
+            print(f"Number of poll requests: {self.num_poll_reqs}")
+            print("*********************************")
+            print(f"Elapsed time: {time.time() - self.start_time}")
+            time.sleep(5)
 
 
-if __name__=="__main__":
-    fxc = FuncXClient()
-    main(fxc, '71509922-996f-4559-b488-4588f06f0925')
+perf_orch = test_orch()
+
+for i in range(14):
+    thr = threading.Thread(target=perf_orch.send_batches_thr_loop, args=())
+    thr.start()
+    print(f"Started the {i}th task push thread...")
+
+for i in range(8):
+    thr = threading.Thread(target=perf_orch.polling_loop, args=())
+    thr.start()
+    print(f"Started the {i}th result thread...")
+
+thr = threading.Thread(target=perf_orch.stats_loop, args=())
+thr.start()
