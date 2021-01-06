a5925c8a9a5a1eae31ff0130beb8bb4ec212b3ef
tskluzac <skluzacek@uchicago.edu>
2021-01-05 20:15:07 -0600

    Clean up obsolete test scripts and ADDING THETA SCALE TESTING.

--- extractors/utils/mappings.py
+++ extractors/utils/mappings.py
@@ -7,5 +7,11 @@ mapping = {
          'container_type': 'singularity',
          'ep_id': '7faf21be-4667-447c-a96d-4dbe14875cf1',
          'data_path': '/project2/chard/skluzacek/inposoutcars/'},
+    'xtract-matio::theta':
+        {'func_uuid': 'a30f90fd-15ad-47c6-bf93-b4f4f753d3a8',  # TODO: update this.
+         'location': 'xtract-matio.img',
+         'container_type': 'singularity',
+         'ep_id': 'daf8b643-2bdb-47a8-9ba2-c338d8e294b4',
+         'data_path': '/projects/CSC249ADCD01/skluzacek/'},
     'xtract-matio::riverk8s': "TODO",
 }
--- tests/test_sublist_generator.py
+++
@@ -1,9 +0,0 @@
-
-from orchestrator.orch_utils.utils import create_list_chunks
-
-ls = [1, 2, 3, 4, 5, 6]
-
-x = create_list_chunks(ls, 3)
-
-for item in x:
-    print(item)
