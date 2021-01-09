9106e003b7fd59113dec5745cb1b0eba356041be
tskluzac <skluzacek@uchicago.edu>
2021-01-08 23:26:34 -0600

    Cleaning up the logs with one neat trick.

--- extractors/utils/mappings.py
+++ extractors/utils/mappings.py
@@ -9,9 +9,9 @@ mapping = {
          'data_path': '/project2/chard/skluzacek/inposoutcars/'},
     'xtract-matio::theta':
         {'func_uuid': 'a30f90fd-15ad-47c6-bf93-b4f4f753d3a8',  # TODO: update this.
-         'location': 'xtract-matio.img',
+         'location': '/projects/CSC249ADCD01/skluzacek/containers/xtract-matio.img',
          'container_type': 'singularity',
-         'ep_id': 'daf8b643-2bdb-47a8-9ba2-c338d8e294b4',
+         'ep_id': '020c40c1-5a5d-46b3-8e27-a7dc0073a8ff',
          'data_path': '/projects/CSC249ADCD01/skluzacek/'},
     'xtract-matio::riverk8s': "TODO",
 }
