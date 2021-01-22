4081a8e1a3993228385dbd0289b0b4399efc48e8
Ubuntu <ubuntu@ip-172-31-44-164.ec2.internal>
2021-01-22 19:05:44 +0000

    making changes to improve throughput

--- extractors/utils/mappings.py
+++ extractors/utils/mappings.py
@@ -11,7 +11,7 @@ mapping = {
         {'func_uuid': 'abec00cf-efd3-492e-a0c3-5c659dd06cdd',  # TODO: update this.
          'location': '/projects/CSC249ADCD01/skluzacek/containers/xtract-matio.img',
          'container_type': 'singularity',
-         'ep_id': '8f35f4b5-0a51-45da-ac82-270141b802f3',
+         'ep_id': 'c2da39df-5215-44e2-acf3-f936f9fb4073',
          'data_path': '/projects/CSC249ADCD01/skluzacek/'},
     'xtract-matio::riverk8s': "TODO",
 }
