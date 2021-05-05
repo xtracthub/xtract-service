405b5d9b377dccfc0110a6c7c014bb630c730cdf
tskluzac <skluzacek@uchicago.edu>
2021-05-05 18:30:56 -0500

    adding back reqs

--- application.py
+++ application.py
@@ -2,14 +2,14 @@
 from flask import Flask
 
 # Import Blueprints
-# from routes import crawl_bp, extract_bp
+from routes import crawl_bp, extract_bp
 
 
 application = Flask(__name__)
 
 # Register Blueprints for crawls and extractions.
-# application.register_blueprint(crawl_bp.crawl_bp)
-# application.register_blueprint(extract_bp.extract_bp)
+application.register_blueprint(crawl_bp.crawl_bp)
+application.register_blueprint(extract_bp.extract_bp)
 
 
 @application.route('/', methods=['POST', 'GET'])
