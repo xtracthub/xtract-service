18facf96f729b9769f5a3c8cac8f532c34b9215d
Joao Victor Rodrigues <jvsr@uchicago.edu>
2021-08-31 14:28:39 -0500

    working on corrections

--- application.py
+++ application.py
@@ -1,22 +1,16 @@
-
-from flask import Flask
+from flask import Flask, app
 
 # Import Blueprints
-from routes import crawl_bp, extract_bp
+from routes import crawl_bp, extract_bp, configure_bp
 
-import json
-
-from funcx import FuncXClient
 
 application = Flask(__name__)
 
 # Register Blueprints for crawls and extractions.
 application.register_blueprint(crawl_bp.crawl_bp)
 application.register_blueprint(extract_bp.extract_bp)
+application.register_blueprint(configure_bp.configure_bp)
 
-# TODO: Step 1: make a 'configure' blueprint that handles logic that has to do with configuring things.
-# TODO: Step 2: have 'configure' route that does the task described in the doc. AND inputs all of the information from the doc.
-# TODO: Step 3: so you'll want to do Globus Auth to get the auth headers, both for funcX and for the Globus services.
 
 @application.route('/', methods=['POST', 'GET'])
 def xtract_default():
@@ -24,15 +18,7 @@ def xtract_default():
     return "FUNCTIONAL"
 
 
-# @application.route('/configure_ep', methods=['POST'])
-# def config_ep():
-#     r = json.loads(request.json)
-#     fx_ep = r['fx_ep']
-#
-#     fxc = FuncXClient()
-#
-#     return {'status': 200, 'message': 'Endpoint successfully configured!', 'fx_eid': fx_ep}
-
-
 if __name__ == '__main__':
     application.run(debug=True, threaded=True, ssl_context="adhoc")
+    print(application.url_map)
+    
--- routes/configure_bp.py
+++ routes/configure_bp.py
@@ -2,56 +2,39 @@ import pickle
 import json
 from flask import Blueprint
 
-
 """ Routes that have to do with using configuration """
 configure_bp = Blueprint('configure_bp', __name__)
 print(configure_bp.root_path)
 
 
-@configure_bp.route('/configure_funcx/<globus_eid>/<funcx_eid>/<home>', methods=['GET', 'POST', 'PUT'])
-def configure_funcx(globus_eid, funcx_eid, home):
-    from fair_research_login import NativeClient
-    from mdf_toolbox import login
-    import os
+@configure_bp.route('/configure_endpoint/<globus_eid>/<funcx_eid>/<home>', methods=['GET', 'POST', 'PUT'])
+def configure_endpoint(globus_eid, funcx_eid, home=None):
+    from funcx import FuncXClient
 
-    client = NativeClient(client_id='7414f0b4-7d05-4bb6-bb00-076fa3f17cf5')
-    tokens = client.login(
-    requested_scopes=['https://auth.globus.org/scopes/56ceac29-e98a-440a-a594-b41e7a084b62/all',
-                      'urn:globus:auth:scope:transfer.api.globus.org:all',
-                      'https://auth.globus.org/scopes/facd7ccc-c5f4-42aa-916b-a0e270e2c2a9/all'],
-    no_local_server=True,
-    no_browser=True,
-    force=True,)
+    # Get these how?
+    header = {
+        'Authorization': 'authorization token',
+        'Transfer': 'transfer token',
+        'FuncX': 'fx_scope',
+        'Petrel': 'petrel token'}
 
-    auths = login(services=[
-        "data_mdf",
-        "search",
-        "petrel",
-        "transfer",
-        "dlhub",
-        "https://auth.globus.org/scopes/facd7ccc-c5f4-42aa-916b-a0e270e2c2a9/all",
-    ],
-    app_name="Foundry",
-    make_clients=True,
-    no_browser=False,
-    no_local_server=False,)
 
-    fx_scope = 'https://auth.globus.org/scopes/facd7ccc-c5f4-42aa-916b-a0e270e2c2a9/all'
-    headers = {
-        'Authorization': auths['petrel'],
-        'Transfer': auths['transfer'],
-        'FuncX': auths[fx_scope],
-        'Petrel': auths['petrel']}
+    def configure_endpoint_fn(header):
+        import os
+        if not os.path.exists('.xtract/'):
+            os.makedirs('.xtract/')
+        with open('.xtract/config.json', 'w') as f:
+            config = {
+                'header_auth': header,
+                'home': '.xtract/',
+                'globus_eid': globus_eid,
+                'funcx_eid': funcx_eid}
+            json.dump(config, f)
+            return {'status': 'success'}
 
-    print(headers)
+    fxc = FuncXClient()
+    fxc.register_endpoint(funcx_eid)
+    fxc.register_function(configure_endpoint_fn(header))
+    fxc.run()
 
-    if not os.path.exists('.xtract/'):
-        os.makedirs('.xtract/')
-    with open('.xtract/config.pickle', 'wb') as f:
-        config = {
-            'header_auth': headers,
-            'home': '.xtract/',
-            'globus_eid': globus_eid,
-            'funcx_eid': funcx_eid}
-        pickle.dump(config, f)
-        return {'status': 'success'}
+    return
