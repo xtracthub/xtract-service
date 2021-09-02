6a0c717752d45359abc02a0c48c086685a8bf92e
Joao Victor Rodrigues <joaovictor@joaovictorMBP.local>
2021-09-02 17:27:12 -0500

    corrected configure_bp.py

--- routes/configure_bp.py
+++ routes/configure_bp.py
@@ -1,11 +1,16 @@
 from flask import Blueprint, request
+import json
+import os
+from globus_sdk import auth
+
+from werkzeug.datastructures import Headers
 
 
 """ Routes that have to do with using configuration """
 configure_bp = Blueprint('configure_bp', __name__)
 
 
-def configure_endpoint_function(headers, funcx_eid, globus_eid=None, home=None):
+def configure_endpoint_function(header_auth, funcx_eid, globus_eid, home):
     import os
     import json
     if not os.path.exists('.xtract/'):
@@ -15,14 +20,14 @@ def configure_endpoint_function(headers, funcx_eid, globus_eid=None, home=None):
             'funcx_eid': funcx_eid, 
             'globus_eid': globus_eid,
             'home': f'.xtract/{home}',
-            'header_auth': headers,
+            'header_auth': header_auth,
             }
         json.dump(config, f)
         return {'status': 'success'}
 
 
 @configure_bp.route('/configure_ep/<funcx_eid>', methods=['GET', 'POST'])
-def configure_endpoint(funcx_eid):
+def configure_ep(funcx_eid):
     """ Configuring the endpoint means ensuring that all credentials on the endpoint
         are updated/refreshed, and that the Globus + funcX eps are online """
     from funcx import FuncXClient
@@ -32,21 +37,25 @@ def configure_endpoint(funcx_eid):
         return {'status': 200, 'message': 'Endpoint configuration unsuccessful!', 'error': 'missing headers'}
 
     headers = request.headers
-    funcx_eid = headers['funcx_eid']
-    globus_eid = headers['globus_eid']
-    home = headers['home']
-    authorization = headers['authorization']
+    header_auth = {
+        'funcx_eid': headers['funcx_eid'],
+        'globus_eid': headers['globus_eid'],
+        'home': headers['home'],
+        'authorization': headers['authorization']
+        }
 
-    fx_auth = AccessTokenAuthorizer(authorization)
-    # search_auth = AccessTokenAuthorizer(auths['search'])
-    # openid_auth = AccessTokenAuthorizer(auths['openid'])
 
+    fx_auth = AccessTokenAuthorizer(header_auth['authorization'])
     fxc = FuncXClient(fx_authorizer=fx_auth)
-        # search_authorizer=search_auth,
-        # openid_authorizer=openid_auth)
     ep_uuid = funcx_eid
     func_uuid = fxc.register_function(configure_endpoint_function)
-    task_id = fxc.run(headers, funcx_eid, globus_eid, home, function_id=func_uuid, endpoint_id=ep_uuid)
-    print(fxc.get_result(task_id=task_id))
+    task_id = fxc.run(header_auth, 
+        header_auth['funcx_eid'], 
+        header_auth['globus_eid'], 
+        header_auth['home'], 
+        function_id=func_uuid, 
+        endpoint_id=ep_uuid)
+
+    # print(fxc.get_result(task_id=task_id))
 
     return {'status': 200, 'message': 'Endpoint successfully configured!', 'funcx_eid': funcx_eid}
