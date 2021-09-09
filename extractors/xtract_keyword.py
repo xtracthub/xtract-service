feab26d07b62d8a8da729aa618e50f062570dab7
Joao Victor Rodrigues <jvsr@uchicago.edu>
2021-09-09 16:55:44 -0500

    iimplemented first version xtract_keyword

--- extractors/xtract_keyword.py
+++ extractors/xtract_keyword.py
@@ -1,70 +1,83 @@
-
 from extractors.extractor import Extractor
+from extractors.utils.base_event import create_event
 
 
 class KeywordExtractor(Extractor):
 
     def __init__(self):
-
         super().__init__(extr_id=None,
-                         func_id="d5a09aee-927f-4d6e-9ee4-5ad461d9148b",
-                         extr_name="xtract-keyword",
+                         func_id="833f6271-e03c-4ac5-bc32-64eba7f13460",
+                         extr_name="xtract-tabular",
                          store_type="ecr",
-                         store_url="039706667969.dkr.ecr.us-east-1.amazonaws.com/xtract-keyword:latest")
-        super().set_extr_func(keyword_extract)
+                         store_url="039706667969.dkr.ecr.us-east-1.amazonaws.com/xtract-tabular:latest")
 
-# def execute_extractor():
-# event is a 'context' for an extractor to run on
+    def create_event(self,
+                     family_batch,
+                     ep_name,
+                     xtract_dir,
+                     sys_path_add,
+                     module_path,
+                     metadata_write_path,
+                     recursion_depth=None):
 
-def keyword_extract(event):
+        event = create_event(family_batch=family_batch,
+                             ep_name=ep_name,
+                             xtract_dir=xtract_dir,
+                             sys_path_add=sys_path_add,
+                             module_path=module_path,
+                             metadata_write_path=metadata_write_path)
 
-    import sys
-    import time
+        return event
 
-    from xtract_sdk.downloaders import GoogleDriveDownloader
+    # def keyword_extract(event):
 
-    t0 = time.time()
+    #     import sys
+    #     import time
 
-    sys.path.insert(1, '/')
-    import xtract_keyword_main
-    # from exceptions import RemoteExceptionWrapper, HttpsDownloadTimeout, ExtractorError, PetrelRetrievalError
+    #     from xtract_sdk.downloaders import GoogleDriveDownloader
 
-    creds = event["creds"]
-    family_batch = event["family_batch"]
+    #     t0 = time.time()
 
-    downloader = GoogleDriveDownloader(auth_creds=creds)
+    #     sys.path.insert(1, '/')
+    #     import xtract_keyword_main
+    #     # from exceptions import RemoteExceptionWrapper, HttpsDownloadTimeout, ExtractorError, PetrelRetrievalError
 
-    ta = time.time()
-    try:
-        downloader.batch_fetch(family_batch=family_batch)
-    except Exception as e:
-        return {'exception': e, 'files': family_batch.file_ls}
-    tb = time.time()
+    #     creds = event["creds"]
+    #     family_batch = event["family_batch"]
 
-    file_paths = downloader.success_files
+    #     downloader = GoogleDriveDownloader(auth_creds=creds)
 
-    if len(file_paths) == 0:
-        return {'family_batch': family_batch, 'error': True, 'tot_time': time.time()-t0,
-                'err_msg': "unable to download files"}
+    #     ta = time.time()
+    #     try:
+    #         downloader.batch_fetch(family_batch=family_batch)
+    #     except Exception as e:
+    #         return {'exception': e, 'files': family_batch.file_ls}
+    #     tb = time.time()
 
-    for family in family_batch.families:
-        keyword_mimetype = family.files[0]['mimeType']
-        is_pdf = True if 'pdf' in keyword_mimetype.lower() else False
-        file_path = family.files[0]['path']
+    #     file_paths = downloader.success_files
 
-        new_mdata = xtract_keyword_main.extract_keyword(file_path, pdf=is_pdf)
+    #     if len(file_paths) == 0:
+    #         return {'family_batch': family_batch, 'error': True, 'tot_time': time.time()-t0,
+    #                 'err_msg': "unable to download files"}
 
-        # TODO: move this sort of thing
-        # Here we remove the empty metadata.
-        if type(new_mdata) is dict():
-            if len(new_mdata['keywords']) == 0:
-                new_mdata = {}
-        if new_mdata is None:
-            new_mdata = {}
+    #     for family in family_batch.families:
+    #         keyword_mimetype = family.files[0]['mimeType']
+    #         is_pdf = True if 'pdf' in keyword_mimetype.lower() else False
+    #         file_path = family.files[0]['path']
 
-        family.metadata = new_mdata
+    #         new_mdata = xtract_keyword_main.extract_keyword(file_path, pdf=is_pdf)
 
-    # TODO: obliterate downloaded files here.
+    #         # TODO: move this sort of thing
+    #         # Here we remove the empty metadata.
+    #         if type(new_mdata) is dict():
+    #             if len(new_mdata['keywords']) == 0:
+    #                 new_mdata = {}
+    #         if new_mdata is None:
+    #             new_mdata = {}
 
-    t1 = time.time()
-    return {'family_batch': family_batch, 'tot_time': t1-t0, 'trans_time': tb-ta}
+    #         family.metadata = new_mdata
+
+    #     # TODO: obliterate downloaded files here.
+
+    #     t1 = time.time()
+    #     return {'family_batch': family_batch, 'tot_time': t1-t0, 'trans_time': tb-ta}
