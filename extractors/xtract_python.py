aff5cda985621806f4dc7c95127f6156ada1689a
Joao Victor Rodrigues <jvsr@uchicago.edu>
2021-09-14 18:55:35 -0500

    added xtract-python as event-based

---
+++ extractors/xtract_python.py
@@ -0,0 +1,30 @@
+from extractors.extractor import Extractor
+from extractors.utils.base_event import create_event
+
+
+class PythonExtractor(Extractor):
+
+    def __init__(self):
+        super().__init__(extr_id=None,
+                         func_id="", # TODO: fill in once func is registered
+                         extr_name="xtract-python",
+                         store_type="", # TODO: fill in once func is registered
+                         store_url="") # TODO: fill in once func is registered
+
+    def create_event(self,
+                     family_batch,
+                     ep_name,
+                     xtract_dir,
+                     sys_path_add,
+                     module_path,
+                     metadata_write_path,
+                     recursion_depth=None):
+
+        event = create_event(family_batch=family_batch,
+                             ep_name=ep_name,
+                             xtract_dir=xtract_dir,
+                             sys_path_add=sys_path_add,
+                             module_path=module_path,
+                             metadata_write_path=metadata_write_path)
+
+        return event
