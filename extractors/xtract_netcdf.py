f728e4080c4c7b57e36e4de7242e4f1f91d21529
Joao Victor Rodrigues <jvsr@uchicago.edu>
2021-09-14 19:25:58 -0500

    added event-based extraction to NetCDF

--- extractors/xtract_netcdf.py
+++ extractors/xtract_netcdf.py
@@ -1,4 +1,5 @@
 from extractors.extractor import Extractor
+from extractors.utils.base_event import create_event
 
 
 class NetCDFExtractor(Extractor):
@@ -9,67 +10,84 @@ class NetCDFExtractor(Extractor):
                          extr_name="xtract-netcdf",
                          store_type="ecr",
                          store_url="039706667969.dkr.ecr.us-east-1.amazonaws.com/xtract-netcdf:lateset")
-        super().set_extr_func(netcdf_extract)
+
+    def create_event(self,
+                     family_batch,
+                     ep_name,
+                     xtract_dir,
+                     sys_path_add,
+                     module_path,
+                     metadata_write_path,
+                     recursion_depth=None):
+
+        event = create_event(family_batch=family_batch,
+                             ep_name=ep_name,
+                             xtract_dir=xtract_dir,
+                             sys_path_add=sys_path_add,
+                             module_path=module_path,
+                             metadata_write_path=metadata_write_path)
+
+        return event
 
 
-def netcdf_extract(event):
-    """Extract metadata from netcdf data.
+# def netcdf_extract(event):
+#     """Extract metadata from netcdf data.
 
-    Parameters
-    ----------
-    event : dict
-        A dict describing the data and credentials to act on
+#     Parameters
+#     ----------
+#     event : dict
+#         A dict describing the data and credentials to act on
 
-    Returns
-    -------
-    dict : The resulting metadata and timers
-    """
-    import sys
-    import time
-    import os
+#     Returns
+#     -------
+#     dict : The resulting metadata and timers
+#     """
+#     import sys
+#     import time
+#     import os
 
-    from xtract_sdk.downloaders.google_drive import GoogleDriveDownloader
+#     from xtract_sdk.downloaders.google_drive import GoogleDriveDownloader
 
-    t0 = time.time()
+#     t0 = time.time()
 
-    sys.path.insert(1, '/')
-    import xtract_netcdf_main
+#     sys.path.insert(1, '/')
+#     import xtract_netcdf_main
 
-    new_mdata = None
+#     new_mdata = None
 
-    creds = event["creds"]
-    family_batch = event["family_batch"]
+#     creds = event["creds"]
+#     family_batch = event["family_batch"]
 
-    # This should be either 'local' or 'remote'.
-    extract_mode = event["extract_mode"]
+#     # This should be either 'local' or 'remote'.
+#     extract_mode = event["extract_mode"]
 
-    downloader = GoogleDriveDownloader(auth_creds=creds)
-    assert extract_mode in ["remote", "local"], "Invalid extraction mode"
+#     downloader = GoogleDriveDownloader(auth_creds=creds)
+#     assert extract_mode in ["remote", "local"], "Invalid extraction mode"
 
-    if extract_mode == "remote":
-        ta = time.time()
-        downloader.batch_fetch(family_batch=family_batch)
-        tb = time.time()
+#     if extract_mode == "remote":
+#         ta = time.time()
+#         downloader.batch_fetch(family_batch=family_batch)
+#         tb = time.time()
 
-        file_paths = downloader.success_files
-    elif extract_mode == "local":
-        ta = tb = 0  # Set both times to be zero so that transfer time is zero.
-        file_paths = []
+#         file_paths = downloader.success_files
+#     elif extract_mode == "local":
+#         ta = tb = 0  # Set both times to be zero so that transfer time is zero.
+#         file_paths = []
 
-    if len(file_paths) == 0 and file_paths == "remote":
-        return {'family_batch': family_batch, 'error': True, 'tot_time': time.time() - t0,
-                'err_msg': "unable to download files"}
+#     if len(file_paths) == 0 and file_paths == "remote":
+#         return {'family_batch': family_batch, 'error': True, 'tot_time': time.time() - t0,
+#                 'err_msg': "unable to download files"}
 
-    for family in family_batch.families:
-        img_path = family.files[0]['path']
-        # return img_path
+#     for family in family_batch.families:
+#         img_path = family.files[0]['path']
+#         # return img_path
 
-        new_mdata = xtract_netcdf_main.extract_netcdf(img_path)
-        family.metadata = new_mdata
+#         new_mdata = xtract_netcdf_main.extract_netcdf(img_path)
+#         family.metadata = new_mdata
 
-    if extract_mode == "remote":
-        [os.remove(file_path) for file_path in downloader.success_files]
+#     if extract_mode == "remote":
+#         [os.remove(file_path) for file_path in downloader.success_files]
 
-    t1 = time.time()
-    # Return batch
-    return {'family_batch': family_batch, 'tot_time': t1 - t0, 'trans_time': tb - ta}
+#     t1 = time.time()
+#     # Return batch
+#     return {'family_batch': family_batch, 'tot_time': t1 - t0, 'trans_time': tb - ta}
