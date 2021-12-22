ae33af1c8e2de2e3d41cfdd37e97f5369b11256f
tskluzac <skluzacek@uchicago.edu>
2021-12-22 15:56:49 -0600

    Trying a rotating handler.

--- application.py
+++ application.py
@@ -5,8 +5,19 @@ from flask import Flask, request
 from routes import crawl_bp, extract_bp, configure_bp
 from utils.pg_utils import pg_conn
 from scheddy.scheduler import get_fx_client
+from logging.handlers import RotatingFileHandler
+
+import logging
+
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+logger.setLevel(logging.DEBUG)
+handler = RotatingFileHandler('/opt/python/log/application.log', maxBytes=1024,backupCount=5)
+handler.setFormatter(formatter)
+
 
 application = Flask(__name__)
+application.logger.addHandler(handler)
 
 # Register Blueprints for crawls and extractions.
 application.register_blueprint(crawl_bp.crawl_bp)
@@ -62,7 +73,7 @@ def config_containers():
         '{item}', '{fx_eid}', '{func_uuids[item]}');"""
         cur.execute(in_query_1)
     conn.commit()
-    application.logger.info("TYLER CHECKING IN")
+    logger.info("TYLER CHECKING IN")
     return {'status': 'OK'}
 
 
