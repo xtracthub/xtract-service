81bb9b54e050288f7a3d0d415de38b5c0158ced8
tskluzac <skluzacek@uchicago.edu>
2021-12-23 11:59:02 -0600

    diagnosing tyler g.'s xtract-keyword issue.

--- tests/extractors_at_compute_facilities/facility_fx_eps.py
+++ tests/extractors_at_compute_facilities/facility_fx_eps.py
@@ -1,5 +1,5 @@
 
 facility_fx_eps = {
     "jetstream": {'fx_ep': '6b3a1745-5e0e-4c60-82db-0faac6cc246f',
-                  'container_path': "/home/tskluzac/.xtract/.containers"}
+                  'container_path': "/home/tskluzac/containers/XtractContainerLibrary"}
 }
