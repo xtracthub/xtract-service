c913b6000e0229d98db8752483ce428be31e09e6
tskluzac <skluzacek@uchicago.edu>
2022-01-28 14:48:57 -0600

    adding "EXTRACTING" phase to workflow.

--- tests/extractors_at_compute_facilities/facility_fx_eps.py
+++ tests/extractors_at_compute_facilities/facility_fx_eps.py
@@ -1,5 +1,5 @@
 
 facility_fx_eps = {
-    "jetstream": {'fx_ep': '6b3a1745-5e0e-4c60-82db-0faac6cc246f',
+    "jetstream": {'fx_ep': '28dadb13-8474-45e0-864b-684b88518f00',
                   'container_path': "/home/tskluzac/containers/XtractContainerLibrary"}
 }
