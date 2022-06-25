193870049711283a2e44382d8b4304237e87d35c
tskluzac <skluzacek@uchicago.edu>
2022-06-24 22:09:55 -0500

    addition of test endpoint.

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 
 import logging
-from flask import Flask
+from flask import Flask, request
 
 # Import Blueprints
 from routes import crawl_bp, extract_bp, configure_bp
@@ -12,6 +12,7 @@ application.logger.setLevel(logging.DEBUG)
 # TODO: this causes repeat messages (one too verbose, and this one).
 # logging.basicConfig(format='%(message)s')  # remove timestamp, already appended by EB.
 
+
 # Register Blueprints for crawls and extractions.
 application.register_blueprint(crawl_bp.crawl_bp)
 application.register_blueprint(extract_bp.extract_bp)
@@ -21,9 +22,16 @@ application.register_blueprint(configure_bp.configure_bp)
 @application.route('/', methods=['POST', 'GET'])
 def xtract_default():
     """ Return the default information as part of the request. """
-    # TODO: helpful message here that Vas can check.
-    print("HITTING FLASK HOME")
     return {'status': "FUNCTIONAL", 'code': 200}
+
+
+@application.route('/test', methods=['POST', 'GET'])
+def xtract_default():
+    r1 = request.data
+    r2 = request.json
+    print(r1)
+    print(r2)
+    return {'status': "FUNCTIONAL", 'code': 200, 'success': True, 'message': 'testmessage123'}
 
 
 if __name__ == '__main__':
